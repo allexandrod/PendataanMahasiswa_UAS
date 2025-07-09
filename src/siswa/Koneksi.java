f238955537fa304d42cc4214bb3bf4b93dcf17f6
delvinoputra <190609208+allexandrod@users.noreply.github.com>
2025-07-09 16:26:24 +0700

    done

---
+++ src/siswa/Koneksi.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package siswa;
+
+/**
+ *
+ * @author sypri
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:sqlite:data_siswa.db"; // file database lokal
+            return DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println("Koneksi Gagal: " + e.getMessage());
+            return null;
+        }
+    }
+}
